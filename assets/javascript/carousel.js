19e72cf12705283fad2364d84c84974f9a53913e
Ignacio Marzotta <53620877+IgnacioMarzotta@users.noreply.github.com>
2023-09-04 16:51:17 -0300

    Add files via upload

---
+++ assets/javascript/carousel.js
@@ -0,0 +1,54 @@
+//Main Banner
+
+const delay = 3000; //ms
+const slides = document.querySelector(".slides");
+const slidesCount = slides.childElementCount;
+const maxLeft = (slidesCount - 1) * 100 * -1;
+let current = 0;
+
+function changeSlide(next = true) 
+{
+  if (next) 
+  {
+    current += current > maxLeft ? -100 : current * -1;
+  } else 
+  {
+    current = current < 0 ? current + 100 : maxLeft;
+  }
+
+  slides.style.left = current + "%";
+}
+let autoChange = setInterval(changeSlide, delay);
+const restart = function() 
+{
+  clearInterval(autoChange);
+  autoChange = setInterval(changeSlide, delay);
+};
+
+// Controls
+document.querySelector(".next-slide").addEventListener("click", function() 
+{
+  changeSlide();
+  restart();
+});
+document.querySelector(".prev-slide").addEventListener("click", function() 
+{
+  changeSlide(false);
+  restart();
+});
+
+
+
+
+
+
+//Category Carousel
+function feature(e, featureClassName) 
+{
+  var element = document.getElementsByClassName('tab-item');
+  for (var i = 0; i < element.length; i++)
+  {
+    var shouldBeActive = element[i].classList.contains(featureClassName);
+    element[i].classList.toggle('active', shouldBeActive);
+  }
+}
